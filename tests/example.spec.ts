7ee813c5baaf9de2831823bd1c863694c866fba6
igor-lantushenko <82377792+igor-lantushenko@users.noreply.github.com>
2024-09-29 13:54:48 +0200

    add report merge functionality

---
+++ tests/example.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('has title 1', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('has title 2', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
